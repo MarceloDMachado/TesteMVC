79529b1df5b0de75856bdf5a29de08b6c4b7ca97
MarceloDMachado <mrl.marcelodias@gmail.com>
2021-03-04 18:22:32 -0300

    Inclusao da model Department

---
+++ Models/Department.cs
@@ -0,0 +1,30 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string Name)
+        {
+
+        }
+
+        public void setId(int id)
+        {
+            this.Id = id;
+        }
+
+        public void setName(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}
